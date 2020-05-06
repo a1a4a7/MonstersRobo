37e2c046bad14106c9f76da3618df9bb3df42cb9
Ken Ho <ken@Kens-MBP.home>
2020-05-05 23:45:27 -0400

    Updates

---
+++ precache-manifest.19a572181a9ee2b940e4a1aa437a1852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d98f1a8dc0388d4370bb6d6352d094",
+    "url": "/MonstersRobo/index.html"
+  },
+  {
+    "revision": "d0511cf7433bb3523954",
+    "url": "/MonstersRobo/static/css/main.444a24e3.chunk.css"
+  },
+  {
+    "revision": "406f068a88a0e025ec1e",
+    "url": "/MonstersRobo/static/js/2.9c42f95f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRobo/static/js/2.9c42f95f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0511cf7433bb3523954",
+    "url": "/MonstersRobo/static/js/main.d3b2d968.chunk.js"
+  },
+  {
+    "revision": "071563030d7a5ea5fef4",
+    "url": "/MonstersRobo/static/js/runtime-main.ca021471.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MonstersRobo/static/media/logo.5d5d9eef.svg"
+  }
+]);
